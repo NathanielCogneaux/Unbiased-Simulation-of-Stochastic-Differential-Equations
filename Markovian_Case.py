3c629e40841552081c0579403fe8603557e8b495
Nathaniel Cogneaux <Nathaniel@cogneaux.net>
2023-12-26 18:58:33 +0100

    Markovian_Case Review before testing

--- Markovian_Case.py
+++ Markovian_Case.py
@@ -5,32 +5,27 @@
 #              - the drift function μ(t,x) is bounded and continuous in (t,x), uniformly 1/2
 #              Hölder in t and uniformly Lipschitz in x, for some constant L > 0
 
-# In this code we use the Hungarian notation as a convention to name the variables
-
-
 import numpy as np
-
-
 
 
 #We introduce a random discrete time grid with β > 0 a fixed positive constant,
 #(τ_i)i>0 be a sequence of i.i.d. E(β)-exponential random variables.
-def RandomTimeGrid(fBeta, nSamples, nT):
+def RandomTimeGrid(Beta, nSamples, T):
     # Generate i.i.d. exponential random variables
-    arrTau = np.random.exponential(scale=1/fBeta, size=nSamples)
+    arrTau = np.random.exponential(scale=1/Beta, size=nSamples)
 
     # Create the time grid (T_k)k≥0
-    arrT = np.minimum(np.cumsum(tau), nT)
+    arrT = np.minimum(np.cumsum(arrTau), T)
 
-    # Compute N_t
-    nN_t = np.argmax(arrT >= nT) - 1
+    # Compute N_T
+    N_T = np.argmax(arrT >= T) - 1
 
-    return arrT, nN_t
+    return arrT, N_T
 
 
-def Unbiased_Simulation_Markovian_Case(funcG, arrX0, funcMu, arrSigma, fBeta, nSamples, nT, nDim):
+def Unbiased_Simulation_Markovian_Case(funcG, arrX0, funcMu, arrSigma, Beta, nSamples, T, nDim):
     # Get a random discrete time grid
-    arrTimeGrid, nN_t = RandomTimeGrid(fBeta, nSamples, nT)
+    arrTimeGrid, N_T = RandomTimeGrid(Beta, nSamples, T)
 
     # Get the number of steps for the Euler Scheme
     nSteps = len(arrTimeGrid)
@@ -49,24 +44,24 @@ def Unbiased_Simulation_Markovian_Case(funcG, arrX0, funcMu, arrSigma, fBeta, nS
     arrDeltaT = np.diff(arrTimeGrid)
 
     # Euler scheme loop
-    for k in range(nN_t + 1):
-        fMuValue_k = funcMu(arrTimeGrid[k], arrX_hat[k])
+    for k in range(N_T + 1):
+        MuValue_k = funcMu(arrTimeGrid[k], arrX_hat[k])
 
         # Euler scheme formula
-        arrX_hat[k + 1] = arrX_hat[k] + fMuValue_k * arrDeltaT[k + 1] + arrSigma * arrDeltaW[k + 1]
+        arrX_hat[k + 1] = arrX_hat[k] + MuValue_k * arrDeltaT[k + 1] + arrSigma * arrDeltaW[k + 1]
 
-    if nN_t > 0 :
+    if N_T > 0 :
         # Initialize the products of the W^1_k of the estimator
-        fProdW1 = 1
+        prodW1 = 1
         arrSigma_transpose_inv = np.linalg.inv(arrSigma.transpose())
         # W^1_k loop
-        for k in range(1, nN_t + 1):
-            fProdW1 *= ((funcMu(arrTimeGrid[k], arrX_hat[k]) - funcMu(arrTimeGrid[k-1], arrX_hat[k-1]))*arrSigma_transpose_inv*arrDeltaW[k + 1])/arrDeltaT[k + 1]
+        for k in range(1, N_T + 1):
+            prodW1 *= ((funcMu(arrTimeGrid[k], arrX_hat[k]) - funcMu(arrTimeGrid[k-1], arrX_hat[k-1]))*arrSigma_transpose_inv*arrDeltaW[k + 1])/arrDeltaT[k + 1]
 
-        Psi_hat = np.exp(fBeta*nT)*(funcG(arrX_hat[-1]) - funcG(arrX_hat[nN_t]))*fBeta**(-nN_t)*fProdW1
+        Psi_hat = np.exp(Beta*T)*(funcG(arrX_hat[-1]) - funcG(arrX_hat[N_T]))*Beta**(-N_T)*prodW1
 
     else :
-        Psi_hat = np.exp(fBeta*nT)*funcG(arrX_hat[-1])
+        Psi_hat = np.exp(fBeta*T)*funcG(arrX_hat[-1])
 
     return Psi_hat
 

5884d96dd01f9af10b4697967e8e9f9799abdf1e
Nathaniel Cogneaux <Nathaniel@cogneaux.net>
2023-12-28 12:10:16 +0100

    1d version of Markovian

--- Markovian_Case.py
+++ Markovian_Case.py
@@ -9,6 +9,7 @@ import numpy as np
 
 #We introduce a random discrete time grid with β > 0 a fixed positive constant,
 #(τ_i)i>0 be a sequence of i.i.d. E(β)-exponential random variables.
+
 def RandomTimeGrid(Beta, T):
     # Initialise the random time grid
     arrT = [0]
@@ -17,11 +18,13 @@ def RandomTimeGrid(Beta, T):
     while sumTau < T:
         arrT.append(sumTau)
         sumTau += np.random.exponential(1/Beta)
+
     N_T=len(arrT)-1
     arrT.append(T)
+
     return arrT, N_T
 
-
+'''
 def Unbiased_Simulation_Markovian_Case(funcG, arrX0, funcMu, arrSigma, Beta, T, nDim):
     # Get a random discrete time grid
     arrTimeGrid, N_T = RandomTimeGrid(Beta, T)
@@ -45,7 +48,7 @@ def Unbiased_Simulation_Markovian_Case(funcG, arrX0, funcMu, arrSigma, Beta, T, 
         MuValue_k = funcMu(arrTimeGrid[k], arrX_hat[k])
 
         # Euler scheme formula
-        arrX_hat[k + 1] = arrX_hat[k] + MuValue_k * arrDeltaT[k] + arrSigma @ arrDeltaW[k]
+        arrX_hat[k + 1] = arrX_hat[k] + arrDeltaT[k] * MuValue_k + arrSigma @ arrDeltaW[k]
 
     if N_T > 0 :
         # Initialize the products of the W^1_k of the estimator
@@ -66,7 +69,49 @@ def Unbiased_Simulation_Markovian_Case(funcG, arrX0, funcMu, arrSigma, Beta, T, 
         Psi_hat = np.exp(Beta*T)*funcG(arrX_hat[-1])
 
     return Psi_hat
+'''
 
+def Unbiased_Simulation_Markovian_Case_1D(funcG, X0, funcMu, Sigma, Beta, T):
+    # Get a random discrete time grid
+    arrTimeGrid, N_T = RandomTimeGrid(Beta, T)
+
+    # Compute (DeltaT_k)k≥0
+    arrDeltaT = np.diff(arrTimeGrid)
+
+    # Initialize array to store X_hat values
+    X_hat = np.zeros(N_T + 2)
+
+    # Set initial value
+    X_hat[0] = X0
+
+    # Simulate the Delta of the d-dimensional Brownian motion W
+    arrDeltaW = np.zeros(N_T+1)
+    for i in range(0, N_T + 1):
+        arrDeltaW[i] = np.random.normal(loc=0.0, scale=arrDeltaT[i])
+
+    # Euler scheme loop
+    for k in range(N_T+1):
+        MuValue_k = funcMu(arrTimeGrid[k], X_hat[k])
+
+        # Euler scheme formula
+        X_hat[k + 1] = X_hat[k] + arrDeltaT[k] * MuValue_k + Sigma * arrDeltaW[k]
+
+    if N_T > 0 :
+        # Initialize the products of the W^1_k of the estimator
+        prodW1 = 1
+
+        Sigma_transpose_inv = 1/Sigma
+
+        # W^1_k loop
+        for k in range(N_T+1):
+            prodW1 *= ((funcMu(arrTimeGrid[k+1],X_hat[k+1]) - funcMu(arrTimeGrid[k], X_hat[k]))*Sigma_transpose_inv*arrDeltaW[k])/arrDeltaT[k]
+
+        Psi_hat = np.exp(Beta*T)*(funcG(X_hat[-1]) - funcG(X_hat[N_T]))*Beta**(-N_T)*prodW1
+
+    else :
+        Psi_hat = np.exp(Beta*T)*funcG(X_hat[-1])
+
+    return Psi_hat
 
 
 
@@ -75,7 +120,7 @@ def MC_estimator(funcG, arrX0, funcMu, arrSigma, Beta, T, nDim, nSamples):
     psi_hats=np.zeros(nSamples)
 
     for i in range(nSamples):
-        psi_hats[i] = Unbiased_Simulation_Markovian_Case(funcG, arrX0, funcMu, arrSigma, Beta, T, nDim)
+        psi_hats[i] = Unbiased_Simulation_Markovian_Case_1D(funcG, arrX0, funcMu, arrSigma, Beta, T)
 
     p=np.mean(psi_hats)
     s=np.std(psi_hats)
--- Numerical Examples.py
+++ Numerical Examples.py
@@ -5,15 +5,12 @@ import Euler_Scheme
 
 # First Example
 # V0 := E[(ST − K)+]
-# where X is defined by the SDE
-# X0 = 0, dXt = (0.1*(np.sqrt(np.min(M, Xt)), μ0 cos(Xt)dt + 0.5dWt
-#X0 = 0, dXt = 
 
-
+'''
 # Parameters:
 nDim = 1
 X_0 = 0
-Beta = 0.1 # Beta constant
+Beta = 0.05 # Beta constant
 Sigma_0 = np.array([0.5])
 M = 4 # large
 K = 1 # strike
@@ -21,15 +18,42 @@ T = 1
 N = 10**5
 EulerScheme_mSteps = 10
 
-def funcMu (t,x) :
-    return 0.1 * (np.sqrt(np.min([M, np.exp(x[0])])) - 1.0) - 0.125
+def funcMu (t,x):
+    return 0.1 * (np.sqrt(np.minimum(M, np.exp(x))) - 1.0) - 0.125
 def funcSigma (t,x) :
     return Sigma_0
 def funcG (x) :
-    return np.max(0, np.exp(x) - K)
+    return np.maximum(0, np.exp(x) - K)
 
 
-#print(Markovian_Case.Unbiased_Simulation_Markovian_Case(np.sin, X_0, funcMu, Sigma_0, Beta, T, nDim))
+#print(Markovian_Case.Unbiased_Simulation_Markovian_Case(funcG, X_0, funcMu, Sigma_0, Beta, T, nDim))
 print(Markovian_Case.MC_estimator(funcG, X_0, funcMu, Sigma_0, Beta, T, nDim, N))
-print(Euler_Scheme.MC_EulerScheme(funcG, X_0, funcMu, funcSigma, T, nDim, EulerScheme_mSteps, N))
+#print(Euler_Scheme.MC_EulerScheme(funcG, X_0, funcMu, funcSigma, T, nDim, EulerScheme_mSteps, N))
+'''
 
+
+
+# TEST 1D ALONE
+
+# Parameters:
+nDim = 1
+X_0 = 0
+Beta = 0.1 # Beta constant
+Sigma_0 = 0.5
+M = 4 # large
+K = 1 # strike
+T = 1
+N = 10**5
+EulerScheme_mSteps = 10
+
+def funcMu (t,x):
+    return 0.1 * (np.sqrt(np.min([M, np.exp(x)])) - 1.0) - 0.125
+def funcSigma (t,x) :
+    return Sigma_0
+def funcG (x) :
+    return np.max([0, np.exp(x) - K])
+
+
+#print(Markovian_Case.Unbiased_Simulation_Markovian_Case(funcG, X_0, funcMu, Sigma_0, Beta, T, nDim))
+print(Markovian_Case.MC_estimator(funcG, X_0, funcMu, Sigma_0, Beta, T, nDim, N))
+#print(Euler_Scheme.MC_EulerScheme(funcG, X_0, funcMu, funcSigma, T, nDim, EulerScheme_mSteps, N))

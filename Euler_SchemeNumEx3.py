7b09ceca6a3f754cb64efb7f26f59acd0998b2af
paul-emile27 <paulemile.galine@hotmail.fr>
2023-12-28 12:16:24 +0100

    commit test time grid

--- Euler_SchemeNumEx3.py
+++ Euler_SchemeNumEx3.py
@@ -36,3 +36,50 @@ print("Statistical error:", statistical_error)
 '''
 
 
+
+"""
+import numpy as np
+
+def RandomTimeGrid(Beta, T):
+    # Initialise the random time grid
+    arrT = [0]
+    sumTau = np.random.exponential(1/Beta)
+    # get Nt := max{k : Tk < t}
+    while sumTau < T:
+        arrT.append(sumTau)
+        sumTau += np.random.exponential(1/Beta)
+    N_T=len(arrT)-1
+    arrT.append(T)
+    return arrT, N_T
+def construct_time_grid_and_NT(T, beta):
+    time_grid = [0]
+    while True:
+        tau = np.random.exponential(1 / beta)
+        next_time = time_grid[-1] + tau
+
+        if next_time >= T:
+            NT = len(time_grid) - 1  # Calculate N_T before adding T
+            time_grid.append(T)
+            break
+
+        time_grid.append(next_time)
+
+    return time_grid, NT
+
+
+
+# Test with same parameters
+T_test, beta_test = 1.0, 0.05
+
+
+# Using construct_time_grid_and_NT
+time_grid_construct, NT_construct = construct_time_grid_and_NT(T_test, beta_test)
+print("construct_time_grid_and_NT - Time Grid:", time_grid_construct, "N_T:", NT_construct)
+
+# Using RandomTimeGrid
+time_grid_random, NT_random = RandomTimeGrid(beta_test, T_test)
+print("RandomTimeGrid - Time Grid:", time_grid_random, "N_T:", NT_random)
+
+
+
+"""

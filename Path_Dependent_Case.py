82923e1ed851f13d47b18673aa0586061cd1b20c
Nathaniel Cogneaux <Nathaniel@cogneaux.net>
2024-01-02 13:39:58 +0100

    2 problems remaining + Working on Path dependent case

--- Path_Dependent_Case.py
+++ Path_Dependent_Case.py
@@ -33,61 +33,55 @@ def BrownianMotionSimulation_Interval(Beta, t1, t2):
     return N_t1t2, arr_t1t2, arrDelta_t1t2, arrDeltaW_t1t2
 
 
-def funcMu_k(k, lInitialConditions, t, x, numIter):
-    lX = lInitialConditions.copy()
+def funcMu_k(k, lX_ti, t, x, numIter):
+    lX = lX_ti[:k] ########### PAS SUR QUE CE SOIT NECESSAIRE
     for i in range(k, numIter):
         lX.append(x)
     return funcMu(t, lX)
 
-# exempel de funcMu possible
+# exemple de funcMu possible
 def funcMu(t, lX):
     return(np.sum(lX)/len(lX))
 
 
 def Unbiased_Simulation_Path_Dependent_Case_1D(funcG, X0, funcMu, Sigma, Beta, lTimeIntervals):
-    # a noter la modif sur funcMU
-    # donc on doit garder les dernieres valeurs comme valeurs initiales pour funcMU !
-    numIter = len(lTimeIntervals)
-    lInitialConditions = [X0]
 
-    for index_ti in range(numIter):
-        N_t1t2, arr_t1t2, arrDelta_t1t2, arrDeltaW_t1t2 = BrownianMotionSimulation_Interval(Beta, lTimeIntervals[index_ti], lTimeIntervals[index_ti+1])
+    numIter = len(lTimeIntervals)
+    lX_ti = []
+
+    # We respect the index notations
+    for k in range(numIter-1):
+        N_t1t2, arr_t1t2, arrDelta_t1t2, arrDeltaW_t1t2 = BrownianMotionSimulation_Interval(Beta, lTimeIntervals[k], lTimeIntervals[k+1])
 
         # Initialize array to store X_tilde values
         arrX_tilde = np.zeros(N_t1t2 + 2)
 
         # Set initial value
-        arrX_tilde[0] = X0
+        arrX_tilde[0] = X0 ################### ATTENTION MUST CHANGE EACH TIMES
 
         # local Euler scheme loop on [tk, tk+1]
-        for k in range(N_t1t2+1):
+        for j in range(N_t1t2+1):
             # Euler scheme formula
-            arrX_tilde[k+1] = arrX_tilde[k] + arrDelta_t1t2[k] * funcMu_k(arr_t1t2[k], arrX_hat[k]) + Sigma * arrDeltaW_t1t2[k]
+            arrX_tilde[j+1] = arrX_tilde[j] + arrDelta_t1t2[j] * funcMu_k(k, lX_ti, arr_t1t2[j], arrX_tilde[j], numIter) + Sigma * arrDeltaW_t1t2[j]
+
+        lX_ti.append(arrX_tilde[-1])
+
+        if N_t1t2 > 0:
+            # Initialize the products of the W^1_k of the estimator
+            prodW1 = 1
+            Sigma_transpose_inv = 1/Sigma
+            # W^1_k loop
+            for j in range(1, N_t1t2 + 1):
+                prodW1 *= ((funcMu_k(k, lX_ti, arr_t1t2[j], arrX_tilde[j], numIter) - funcMu_k(k, lX_ti, arr_t1t2[j-1], arrX_tilde[j-1], numIter))*
+                           arrSigma_transpose_inv*arrDeltaW[j])/arrDeltaT[j]
 
 
-    ############################################
+############################################
+            Psi_hat = np.exp(Beta*T)*(funcG(arrX_hat[-1]) - funcG(arrX_hat[N_T]))*Beta**(-N_T)*prodW1
 
-
-
-
-
-    if N_T > 0 :
-        # Initialize the products of the W^1_k of the estimator
-        prodW1 = 1
-
-        if nDim > 1:
-            arrSigma_transpose_inv = np.linalg.inv(arrSigma.transpose())
         else:
-            arrSigma_transpose_inv = 1/arrSigma
+            Psi_hat = np.exp(Beta*T)*funcG(arrX_hat[-1])
 
-        # W^1_k loop
-        for k in range(1, N_T):
-            prodW1 *= ((funcMu(arrTimeGrid[k], arrX_hat[k]) - funcMu(arrTimeGrid[k-1], arrX_hat[k-1]))*arrSigma_transpose_inv*arrDeltaW[k])/arrDeltaT[k]
-
-        Psi_hat = np.exp(Beta*T)*(funcG(arrX_hat[-1]) - funcG(arrX_hat[N_T]))*Beta**(-N_T)*prodW1
-
-    else :
-        Psi_hat = np.exp(Beta*T)*funcG(arrX_hat[-1])
 
     return Psi_hat
 
@@ -137,8 +131,7 @@ def MC_estimator(funcG, arrX0, funcMu, arrSigma, Beta, T, nDim, nSamples):
     psi_hats=np.zeros(nSamples)
 
     for i in range(nSamples):
-        psi_hats[i] = Unbiased_Simulation_Markovian_Case_1D(funcG, arrX0, funcMu, arrSigma, Beta, T)
-        #psi_hats[i] = Unbiased_Simulation_Markovian_Case(funcG, arrX0, funcMu, arrSigma, Beta, T, nDim)
+        psi_hats[i] = Unbiased_Simulation_Path_Dependent_Case_1D((funcG, X0, funcMu, Sigma, Beta, lTimeIntervals))
 
     p=np.mean(psi_hats)
     s=np.std(psi_hats)
